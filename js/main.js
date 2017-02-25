58d254eb735d66bebde142c07248a3dcebc78b14
ojnatalie <elopixelo@gmail.com>
2017-02-25 13:35:05 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+ $(function() {
+     $('.arrow').on('click', function() {
+         $('html, body').animate({
+             scrollTop: 720
+         }, 1000);
+     });
+ });
